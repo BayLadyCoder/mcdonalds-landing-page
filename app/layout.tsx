75b85fdf96fc923cf1081876f142bec4cc3af3fa
BayLadyCoder <rachada_32@hotmail.com>
2023-11-30 20:22:12 -0500

    Update favicon and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "NextJS McDonald's",
+  title: "WcDonald's",
   description: "Designed and inspired by McDonald's",
 };
 
